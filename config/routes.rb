8644f7a9729ca012617671982b38ad7fc7eaef94
George Soderholm <georgesoderholm@gmail.com>
2020-10-09 21:15:48 -0600

    Add a GET route to pets#show

--- config/routes.rb
+++ config/routes.rb
@@ -12,5 +12,6 @@ Rails.application.routes.draw do
   delete '/shelters/:id', to: 'shelters#destroy'
 
   get '/pets', to: 'pets#index'
+  get '/pets/:id', to: 'pets#show'
   get '/shelters/:shelter_id/pets', to: 'pets#index_shelter'
 end
