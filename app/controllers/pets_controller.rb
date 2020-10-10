3f807682b07b9d0b4ac1266b25fcb9e9419a9dc4
George Soderholm <georgesoderholm@gmail.com>
2020-10-09 18:28:44 -0600

    Add a method fo index_shelter to find all the pets inside a shelter

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -2,4 +2,10 @@ class PetsController < ApplicationController
   def index
     @pets = Pet.all
   end
+
+  def index_shelter
+    @shelter = Shelter.find(params[:shelter_id])
+    @pets = @shelter.pets.all
+    # binding.pry
+  end
 end
