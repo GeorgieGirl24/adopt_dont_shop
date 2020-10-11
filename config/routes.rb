7d144c974eb8475493b523407305e18eb454d4ab
George Soderholm <georgesoderholm@gmail.com>
2020-10-10 21:43:15 -0600

    Add a DELETE route to pets#destroy

--- config/routes.rb
+++ config/routes.rb
@@ -13,9 +13,11 @@ Rails.application.routes.draw do
 
   get '/pets', to: 'pets#index'
   get '/pets/:id', to: 'pets#show'
+  get '/pets/:pet_id/edit', to: 'pets#edit'
+  patch '/pets/:pet_id', to: 'pets#update'
+  delete '/pets/:pet_id', to: 'pets#destroy'
+
   get '/shelters/:shelter_id/pets', to: 'pets#index_shelter'
   get '/shelters/:shelter_id/pets/new', to: 'pets#new'
   post '/shelters/:shelter_id/pets', to: 'pets#create'
-  get '/pets/:pet_id/edit', to: 'pets#edit'
-  patch '/pets/:pet_id', to: 'pets#update'
 end
