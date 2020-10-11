6e5118e4d5a1d40b6329eb8e638784606611cead
George Soderholm <georgesoderholm@gmail.com>
2020-10-11 16:21:00 -0600

    Add test to add links to Pets Index and Shelter Index

--- spec/features/shelters/index_spec.rb
+++ spec/features/shelters/index_spec.rb
@@ -16,9 +16,22 @@ RSpec.describe 'Shelters Index' do
     end
 
     it "can see the name of each shelter in the system" do
-      visit "/shelters"
+      visit '/shelters'
+
       expect(page).to have_content(@shelter_1.name)
       expect(page).to have_content(@shelter_2.name)
+    end
+
+    it 'can have a link to see all the pets in the system' do
+      visit '/shelters'
+
+      expect(page).to have_content(@shelter_1.name)
+      expect(page).to have_content(@shelter_2.name)
+
+      expect(page).to have_link('See All Pets')
+      click_link 'See All Pets'
+
+      expect(current_path).to eq('/pets')
     end
   end
 end
