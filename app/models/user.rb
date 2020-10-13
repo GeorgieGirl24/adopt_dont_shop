78f3c15a590331afddeaab4cf1ed317705325e68
George Soderholm <georgesoderholm@gmail.com>
2020-10-13 17:44:10 -0600

    Add validations for name, street address, city, state and zip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
-  
+  validates_presence_of :name
+  validates_presence_of :street_address
+  validates_presence_of :city
+  validates_presence_of :state
+  validates_presence_of :zip
 end
