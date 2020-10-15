0c874406a250215af8120c9905af3bf30e64ad67
George Soderholm <georgesoderholm@gmail.com>
2020-10-15 13:55:20 -0600

    Add a user, shelter and review

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+@user_2 = User.create!(
+  name: 'Natalie Cruz',
+  street_address: '7363 Main St.',
+  city: 'Seattle',
+  state: 'WA',
+  zip: '39482'
+)
+@review = @shelter_1.reviews.create!(title: 'Awesome Staff',
+  rating: 4.7,
+  content: 'I dropped off Fluffy and the staff was so attentive.',
+  image: 'https://imgur.com/gallery/xO0wmxm')
+
+@shelter_1 = Shelter.create!(name: 'Max Fund - Littleton',
+  address: '1234 Broadway St',
+  city: 'Littleton',
+  state: 'CO',
+  zip: '82349')
