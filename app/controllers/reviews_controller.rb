5da5ce6ae0a5383d2b95b954c0901aed9dbb9943
George Soderholm <georgesoderholm@gmail.com>
2020-10-14 18:07:38 -0600

    Add a new method and a create method

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,11 +1,29 @@
 class ReviewsController < ApplicationController
   def show
-    @review = Review.find(params[:review_id])
+    @review = Review.all
   end
 
   def index
+    @shelter = Shelter.find(params[:shelter_id])
+  end
 
-    @review = Review.all
-    # @shelter = Shelter.find(params[:shelter_id])
+  def new
+    # binding.pry
+    @shelter = Shelter.find(params[:shelter_id])
+  end
+
+  def create
+    # binding.pry
+    # @user = User.find(params[:user_id])
+    # shelter = Shelter.find(params[:shelter_id])
+    review = shelter.reviews.new(review_params)
+    review.save
+
+    redirect_to "/shelters/#{@shelter_id}"
+  end
+
+  private
+  def review_params
+    params.permit(:title, :rating, :content, :image, :name, :shelter_id)
   end
 end
