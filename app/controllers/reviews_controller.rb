84e2318f0cf02b9905cb1af1b37e8815cb08fc0d
George Soderholm <georgesoderholm@gmail.com>
2020-10-15 13:56:35 -0600

    Add to our Create method with AR to be able to create a Review using a shleter_id and user_id

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -12,17 +12,16 @@ class ReviewsController < ApplicationController
   end
 
   def create
-    # @user = User.pluck(:name).first
-    @user_name = User.where(name: params[:name])
+    @user_id = User.where(name: params[:name]).pluck(:id).first
     @shelter = Shelter.find(params[:shelter_id])
     review = Review.new(title: params[:title],
       rating: params[:rating],
       content: params[:content],
       image: params[:image],
-      user: @user_name.first,
-      shelter: @shelter)
-      # binding.pry
-    # review = Review.new(review_params, name: @user_name.first, shelter: @shelter)
+      shelter: @shelter,
+      user_id: @user_id)
+
+    # review = Review.new(review_params, user_id: @user_id, shelter: @shelter)
     review.save!
 
     redirect_to "/shelters/#{@shelter.id}"
