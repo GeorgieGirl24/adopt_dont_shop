4dbaa76233f74b54c381aaf5242157b39f9704b3
George Soderholm <georgesoderholm@gmail.com>
2020-10-17 17:33:13 -0600

    Add average_review_score to User Model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
   has_many :reviews
-  
+
   validates_presence_of :name
   validates_presence_of :street_address
   validates_presence_of :city
   validates_presence_of :state
   validates_presence_of :zip
+
+  def average_review_score(reviews)
+    reviews.average(:rating).round(2)
+  end
 end
