c1da3455bfd420d721e6f38740e92157480a7688
George Soderholm <georgesoderholm@gmail.com>
2020-10-17 16:52:54 -0600

    Add tests for two types of flash messages: unknown/missing user and blank field

--- spec/features/reviews/new_spec.rb
+++ spec/features/reviews/new_spec.rb
@@ -27,6 +27,14 @@ RSpec.describe 'Shelter Review Creation' do
          shelter: @shelter_1,
          user_id: @user_2.id
        )
+       @review_2 = @shelter_1.reviews.create!(
+         title: 'Blah',
+         rating: 3.0,
+         content: 'Great',
+         image: 'https://i.imgur.com/FRK6meX.png',
+         shelter: @shelter_1,
+         user_id: @user_2.id
+       )
      end
 
      it 'can see a link to add a new review' do
@@ -37,10 +45,10 @@ RSpec.describe 'Shelter Review Creation' do
 
        expect(current_path).to eq("/shelters/#{@shelter_1.id}/reviews/new")
 
-       fill_in :title, with: 'Wicked haaarrrible!'
-       fill_in :rating, with: 1.2
-       fill_in :content, with: 'The shelters in Boston are much better!'
-       fill_in :image, with: 'https://i.imgur.com/AAP67gc.jpeg'
+       fill_in :title, with: @review_1.title
+       fill_in :rating, with: @review_1.rating
+       fill_in :content, with: @review_1.content
+       fill_in :image, with: @review_1.image
        fill_in :name, with: @user_2.name
        expect(page).to have_button('Submit')
 
@@ -58,19 +66,24 @@ RSpec.describe 'Shelter Review Creation' do
     end
 
     it 'needs to fill in title, rating and/or content, else a flash message will appear' do
-      visit "/shelters/#{@shelter_1.id}"
+      visit "/shelters/#{@shelter_1.id}/reviews/new"
 
-      click_link 'Add a Review'
-      expect(current_path).to eq("/shelters/#{@shelter_1.id}/reviews/new")
+      fill_in :title, with: @review_2.title
+      fill_in :content, with: @review_2.content
+      fill_in :rating, with: ''
+      fill_in :name, with: @user_2.name
 
+      expect(find_field(:content).value).to eq @review_2.content
+
+      expect(find_field(:name).value).to eq @user_2.name
+      expect(page).to have_button('Submit')
       click_button 'Submit'
-      expect(page).to have_content('Required information missing. Request not submitted')
-      # expect(flash[:alert]).to eq('Required information missing. Request not submitted.')
-      # flash[:alert].should_not be_nil
 
-      fill_in :title, with: @review_1.title
-      fill_in :rating, with: @review_1.rating
-      fill_in :content, with: @review_1.content
+      expect(page).to have_content('Required information missing. Request not submitted')
+
+      fill_in :title, with: @review_2.title
+      fill_in :rating, with: @review_2.rating
+      fill_in :content, with: @review_2.content
       fill_in :image, with: 'https://i.imgur.com/AAP67gc.jpeg'
       fill_in :name, with: @user_2.name
       expect(page).to have_button('Submit')
@@ -78,7 +91,33 @@ RSpec.describe 'Shelter Review Creation' do
       click_button 'Submit'
 
       expect(current_path).to eq("/shelters/#{@shelter_1.id}")
-      expect(page).to have_content(@review_1.title)
+      expect(page).to have_content(@review_2.title)
+    end
+
+    it 'can not complete a review if the user is not in the database' do
+      visit "/shelters/#{@shelter_1.id}"
+
+      click_link 'Add a Review'
+      expect(current_path).to eq("/shelters/#{@shelter_1.id}/reviews/new")
+
+      fill_in :title, with: @review_1.title
+      fill_in :rating, with: @review_1.rating
+      fill_in :content, with: @review_1.content
+      fill_in :image, with: 'https://i.imgur.com/AAP67gc.jpeg'
+      fill_in :name, with: 'Godfrey'
+
+      click_button 'Submit'
+      expect(page).to have_content("That name doesn't exisit in our database. You must be a valid user.")
+
+      fill_in :title, with: @review_1.title
+      fill_in :rating, with: @review_1.rating
+      fill_in :content, with: @review_1.content
+      fill_in :image, with: 'https://i.imgur.com/AAP67gc.jpeg'
+      fill_in :name, with: @user_2.name
+      click_button 'Submit'
+
+      expect(current_path).to eq("/shelters/#{@shelter_1.id}")
+      expect(page).to have_content(@user_2.name)
     end
   end
 end
