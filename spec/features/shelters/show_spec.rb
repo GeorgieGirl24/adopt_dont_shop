187e0e459c7cc6340119398f3b2f18e46ab4a24f
George Soderholm <georgesoderholm@gmail.com>
2020-10-17 20:39:18 -0600

    Add test for adding statistics to the Shelter Show Page

--- spec/features/shelters/show_spec.rb
+++ spec/features/shelters/show_spec.rb
@@ -8,12 +8,98 @@ RSpec.describe 'Shelter Show' do
           address: '1234 Broadway St',
           city: 'Littleton',
           state: 'CO',
-          zip: '82349')
+          zip: '82349'
+        )
         @shelter_2 = Shelter.create!(name: 'Humane Society - Thorton',
           address: '12489 120 St',
           city: 'Thorton',
           state: 'CO',
-          zip: '80829')
+          zip: '80829'
+        )
+        @user_1 = User.create!(
+          name: 'Brian',
+          street_address: '123 Medival Dr.',
+          city: 'Northglenn',
+          state: 'CO',
+          zip: '80273'
+        )
+        @user_2 = User.create!(
+          name: 'Natalie Cruz',
+          street_address: '7363 Main St.',
+          city: 'Seattle',
+          state: 'WA',
+          zip: '39482'
+        )
+
+        image_1 = 'https://www.101dogbreeds.com/wp-content/uploads/2015/10/Italian-Greyhuahua.jpg'
+        image_2 = 'https://icatcare.org/app/uploads/2018/09/white-cat-1.png'
+        image_3 = 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/ed/Old_English_Sheepdog_%28side%29.jpg/2560px-Old_English_Sheepdog_%28side%29.jpg'
+        image_4 = 'https://i.imgur.com/xtu2CYnb.jpg'
+        image_5 = 'https://i.imgur.com/rznWo0nb.jpg'
+
+        @pet_1 = Pet.create!(
+          image: image_3,
+          name: 'Mollie',
+          approximate_age: 6,
+          sex: 'Female',
+          shelter_id: "#{@shelter_2.id}"
+        )
+        @pet_2 = Pet.create!(
+          image: image_1,
+          name: 'Misto',
+          approximate_age: 12,
+          sex: 'Male',
+          shelter_id: "#{@shelter_1.id}"
+        )
+        @pet_3 = Pet.create!(
+          image: image_2,
+          name: 'Monkey',
+          approximate_age: 12,
+          sex: 'Female',
+          shelter_id: "#{@shelter_1.id}"
+        )
+        @pet_4 = Pet.create!(
+          image: image_4,
+          name: 'Leslie',
+          approximate_age: 8,
+          sex: 'Female',
+          shelter_id: "#{@shelter_1.id}"
+        )
+        @pet_5 = Pet.create!(
+          image: image_5,
+          name: 'Mr. T',
+          approximate_age: 3,
+          sex: 'Female',
+          shelter_id: "#{@shelter_2.id}"
+        )
+        @review_1 = @shelter_1.reviews.create!(
+          title: 'Awesome Shelter!',
+          rating: '4.5',
+          content: 'I love this place! Amazing service!',
+          image: 'https://i.imgur.com/FRK6meX.png',
+          user_id: @user_1.id
+        )
+        @review_2 = @shelter_2.reviews.create!(
+          title: 'This place is aight',
+          rating: '3.5',
+          content: 'I mean...its aight, whatever.',
+          image: '',
+          user_id: @user_1.id
+        )
+        @review_3 = @shelter_2.reviews.create!(
+          title: 'Loved the care of Sophia',
+          rating: '4.9',
+          content: 'Sophia is an awesome employee. She deserves a raise',
+          image: 'https://i.imgur.com/ETO90Ubb.jpg',
+          user_id: @user_1.id
+        )
+        @review_4 = @shelter_1.reviews.create!(
+          title: 'Loved the care of Sophia',
+          rating: '4.9',
+          content: 'Sophia is an awesome employee. She deserves a raise',
+          image: 'https://i.imgur.com/ETO90Ubb.jpg',
+          user_id: @user_1.id
+        )
       end
 
       it 'can see the shelters information' do
@@ -44,6 +130,18 @@ RSpec.describe 'Shelter Show' do
         click_link 'Back to All Pets'
 
         expect(current_path).to eq('/pets')
+      end
+
+      it 'can see the statistics for the shelter' do
+        visit "/shelters/#{@shelter_1.id}"
+
+        within "#shelter-statistics" do
+          expect(page).to have_content('Total number of pets')
+          expect(page).to have_content('3')
+          expect(page).to have_content('Average review score')
+          expect(page).to have_content('4.7')
+          # expect(page).to have_content('Total number of applications')
+        end
       end
     end
   end
