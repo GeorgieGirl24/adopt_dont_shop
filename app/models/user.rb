e54a5c567183ac7872d892764929031853ea4fee
Brian Liu <brian.b.liu@gmail.com>
2020-10-18 11:05:43 -0600

    Revise methods highest and lowest rated reviews in user model test

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,40 @@ class User < ApplicationRecord
   validates_presence_of :zip
 
   def highest_rated_review
-    @user = User.all.first
-    @user.reviews.order(rating: :desc).pluck(:rating).first
+    highest = self.reviews.order(rating: :desc).first
+    # binding.pry
+    if self.reviews.empty?
+      highest_review = {title: nil,
+        rating: nil,
+        content: nil,
+        image: nil
+      }
+    else
+      highest_review = {
+        title: highest.title,
+        rating: highest.rating,
+        content: highest.content,
+        image: highest.image
+      }
+    end
   end
 
   def lowest_rated_review
-    @user = User.all.first
-    @user.reviews.order(:rating).pluck(:rating).first
+    lowest = self.reviews.order(:rating).first
+    if self.reviews.empty?
+      lowest_review = {
+        title: nil,
+        rating: nil,
+        content: nil,
+        image: nil
+      }
+    else
+      lowest_review = {
+        title: lowest.title,
+        rating: lowest.rating,
+        content: lowest.content,
+        image: lowest.image
+      }
+    end
   end
 end
