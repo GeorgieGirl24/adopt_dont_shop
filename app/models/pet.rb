eeedc430cc968fc95115855e97f5d5863617b59f
Brian Liu <brian.b.liu@gmail.com>
2020-10-18 23:53:39 -0600

    Add method search to pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -4,4 +4,14 @@ class Pet < ApplicationRecord
   has_many :applications, through: :application_pets
 
   validates_presence_of :shelter_id
+
+  def self.search(search)
+    pet = Pet.find_by(name: search)
+    if pet
+      # binding.pry
+      self.where(name: pet.name)
+    else
+      Pet.all
+    end
+  end
 end
