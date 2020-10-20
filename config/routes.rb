a8becbad7256090c8e928e2e1d158f893dbdcf57
George Soderholm <georgesoderholm@gmail.com>
2020-10-19 18:59:48 -0600

    Add a route to update the application

--- config/routes.rb
+++ config/routes.rb
@@ -36,4 +36,6 @@ Rails.application.routes.draw do
   get '/applications/new', to: 'applications#new'
   post '/applications', to: 'applications#create'
   get '/applications/:application_id', to: 'applications#show'
+  get '/applications/:application_id/edit', to: 'applications#edit'
+  patch '/applications/:application_id', to: 'applications#update'
 end
