b505adb7d624f6a163fb0e81a7ab071b47f10817
Brian Liu <brian.b.liu@gmail.com>
2020-10-21 17:53:56 -0600

    Add helper method to application pet model

--- app/models/application_pet.rb
+++ app/models/application_pet.rb
@@ -3,4 +3,12 @@ class ApplicationPet < ApplicationRecord
   belongs_to :pet
 
   validates_presence_of :pet_status, :allow_nil => true
+
+  def pet_name
+    self.pet.name
+  end
+
+  def pet_adoptable?
+    self.pet.adoptable
+  end
 end
