254e0f219d168fdbba2b8be4ef778518e5a21fea
Brian Liu <brian.b.liu@gmail.com>
2020-10-21 17:53:36 -0600

    Refactor applications controller

--- app/controllers/admin_applications_controller.rb
+++ app/controllers/admin_applications_controller.rb
@@ -7,9 +7,19 @@ class AdminApplicationsController < ApplicationController
   def update
     @application = Application.find(params[:application_id])
     @application_pet = ApplicationPet.find(params[:pet_id])
-    @application_pet.update(pet_status: 'Accepted')
+    @application_pet.pet_status == 'Accepted'
     @application_pet.pet.update(adoptable: false)
-  
+    
     redirect_to "/admin/applications/#{@application.id}"
   end
 end
+
+# if @application_pet.pet_status == nil
+#   @application_pet.pet_status == 'Accepted'
+#   if
+#     @application_pet.update(pet_status: 'Accepted')
+#     @application_pet.pet.update(adoptable: false)
+#   elsif @application_pet.pet_status == 'Rejected'
+#     @application_pet.update(pet_status: 'Rejected')
+#   end
+# end
